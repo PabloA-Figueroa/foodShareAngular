3eff9758c1169602fdbe7bb63d10930d62b62ce8
PabloA-Figueroa <apfigueroa3@utpl.edu.ec>
2023-09-17 15:11:02 -0500

    Commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule, ROUTES} from "@angular/router";
+import {InicioComponent} from "./inicio/inicio.component";
+import {SobreNosotrosComponent} from "./sobre-nosotros/sobre-nosotros.component";
+import {ReseniasComponent} from "./resenias/resenias.component";
 
-
-
+const routes: Routes = [
+  {path: 'Inicio', component : InicioComponent},
+  {path: 'Sobre-Nosotros', component : SobreNosotrosComponent},
+  {path: 'Resenias', component : ReseniasComponent},
+  {path: '', redirectTo: '/Inicio', pathMatch: 'full'},
+];
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { HeaderComponent } from './header/header.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { OfertasComponent } from './ofertas/ofertas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from "./app-routing.module";
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ReseniasComponent } from './resenias/resenias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     InicioComponent,
-    OfertasComponent
+    OfertasComponent,
+    SobreNosotrosComponent,
+    ReseniasComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
